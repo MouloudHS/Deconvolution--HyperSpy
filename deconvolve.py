08182ca49dad4e2a14e17443fc4e553a4f9864d5
MouloudHS <mouloud.bahaeeddine@gmail.com>
2022-04-20 17:03:47 +0200

    Simple python script for deconvolving 4D-STEM data

---
+++ deconvolve.py
@@ -0,0 +1,29 @@
+from skimage import measure
+
+def get_psf_from_entropy(signal, p = 0.85):
+    
+    entropy_map = signal.deepcopy()
+    entropy_map.map(measure.shannon_entropy)
+    mean_entropy = entropy_map.data.mean()
+    
+    mean_entropy = entropy_map.data.mean()
+    mask_entropy = entropy_map < p*mean_entropy
+    s_low = signal*mask_entropy
+    psf = s_low.sum()
+    psf = psf/psf.data.max()
+
+    return psf
+
+def deconvolve(image, psf, itr = 50):
+    from skimage.restoration import richardson_lucy
+    
+    norm_const = image.max()
+    image = image/norm_const
+    
+    # Deconvolution
+    image = richardson_lucy(image, psf, iterations=itr)
+    
+    # Multiply by the saved normalization constant
+    image = image * norm_const
+
+    return image
